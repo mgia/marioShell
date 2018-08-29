664e26aabee3f45e62b8f1dadbf4dbd2a46d7d03
Marc G Tan <findmarcg@gmail.com>
2018-08-29 17:05:47 -0400

    Added minishell.h

---
+++ includes/minishell.h
@@ -0,0 +1,11 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <readline/readline.h>
+# include <readline/history.h>
+# include "libft.h"
+
+# define RED	"\x1B[31m"
+# define WHITE	"\x1B[0m"
+# define SHELL " "RED"mario"WHITE"Shell: "
+#endif
