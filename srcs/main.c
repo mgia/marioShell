d2c7211799d5b17a80f9df6d89747253314ef3ec
Marc G Tan <findmarcg@gmail.com>
2018-08-30 12:13:46 -0400

    Added basic structure to handle multi-command

--- srcs/main.c
+++ srcs/main.c
@@ -19,15 +19,58 @@ void	display_cwd(void)
 
 	cwd = getcwd(buff, 4096);
 	ft_putstr(cwd);
-	// free(cwd);
 }
 
-int		main(int ac, char **av)
+void	ft_freestrarr(char **arr)
 {
-	char *line;
+	int	i;
+
+	if (!arr)
+		return ;
+	i = -1;
+	while (arr[++i])
+		free(arr[i]);
+	free(arr);
+	arr = NULL;
+}
+
+// char	**init_env(char **envp)
+// {
+//
+// }
+
+void	execute_command(char **command)
+{
+	int i = -1;
+	while (command[++i])
+		ft_printf("%s ", command[i]);
+	ft_putchar('\n');
+}
+
+void	execute_commands(char **commands)
+{
+	int		i;
+	char	**command;
+
+	i = -1;
+	while (commands[++i])
+	{
+		command = ft_strsplit(commands[i], ' ');
+		rl_bind_key('\t', rl_complete);
+		execute_command(command);
+		ft_freestrarr(command);
+	}
+}
+
+int		main(int ac, char **av, char **envp)
+{
+	char	*line;
+	char	**commands;
 
 	(void)ac;
 	(void)av;
+	(void)envp;
+	// init_env(envp);
 	while (1)
 	{
 		signal(SIGINT, signal_handler);
@@ -36,7 +79,10 @@ int		main(int ac, char **av)
 		if (!line || !*line)
 			continue ;
 		add_history(line);
-		ft_printf("Command: %s\n", line);
+		commands = ft_strsplit(line, ';');
+		free(line);
+		execute_commands(commands);
+		ft_freestrarr(commands);
 	}
 	return (0);
 }
