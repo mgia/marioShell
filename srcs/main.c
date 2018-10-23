a4c193dbfbcf355736befcc45f20c0ae1b925090
Marc G Tan <findmarcg@gmail.com>
2018-10-22 21:35:40 -0400

    Added cd_command

--- srcs/main.c
+++ srcs/main.c
@@ -57,7 +57,16 @@ void	init_env(char **envp)
 		g_envp[i] = ft_strdup(envp[i]);
 }
 
-// char	*check_arg
+char	*find_arg(char *key, int len)
+{
+	int		i;
+
+	i = -1;
+	while (g_envp[++i])
+		if (!ft_strncmp(g_envp[i], key, len))
+			return (g_envp[i] + len + 1);
+	return ("");
+}
 
 /*
 **	Echo Builtin: Handles complete quotation marks ("arg" => arg), expansions
@@ -69,9 +78,9 @@ void	echo_print(char *arg, int len)
 	if (!len)
 		return ;
 	if (len == 1 && arg[0] == '~')
-		ft_putstr("get $HOME");
+		ft_printf("%s", find_arg("HOME", 4));
 	else if (arg[0] == '$')
-		ft_putstr("get $ENV");
+		ft_printf("%s", find_arg(&arg[1], ft_strlen(arg) - 1));
 	else
 		ft_putnstr(arg, len);
 }
@@ -145,7 +154,7 @@ char	**realloc_env(int len)
 	return (res);
 }
 
-void	set_envp(char *key, char *value)
+void	set_envp(char *key, char *value, int print)
 {
 	int		pos;
 	char	*tmp;
@@ -168,7 +177,8 @@ void	set_envp(char *key, char *value)
 		else
 			g_envp[pos] = ft_strjoin(key, "=");
 	}
-	ft_printf("[%d] %s (setenv)\n", pos, g_envp[pos]);
+	if (print)
+		ft_printf("[%d] %s (setenv)\n", pos, g_envp[pos]);
 	free(tmp);
 }
 
@@ -187,7 +197,7 @@ void	set_env(char **arg)
 		ft_putendl("Error: Invalid number of arguments (setenv)");
 		return ;
 	}
-	set_envp(arg[0], arg[1]);
+	set_envp(arg[0], arg[1], 1);
 }
 
 void	unset_envp(int pos)
@@ -226,6 +236,47 @@ void	unset_env(char **args)
 	}
 }
 
+void	print_path(char* path)
+{
+	path = NULL;
+}
+
+void	change_dir(char *path, int print)
+{
+	char	*cwd;
+	char	buf[4096 + 1];
+
+	cwd = getcwd(buf, 4096);
+	if (!chdir(path))
+	{
+		if (print)
+		{
+			print_path(path);
+		}
+		set_envp("OLDPWD", cwd, 0);
+	}
+	else
+	{
+		ft_printf("cd: %s: ", path);
+		if (access(path, F_OK) == -1)
+			ft_putendl("no such file or directory");
+		else if (access(path, R_OK) == -1)
+			ft_putendl("permission denied");
+		else
+			ft_putendl("not a directory");
+	}
+}
+
+void	cd_command(char **args)
+{
+	if (!args[0] || ft_strequ(args[0], "--"))
+		change_dir(find_arg("HOME", 4), 0);
+	else if (ft_strequ(args[0], "-"))
+		change_dir(find_arg("OLDPWD", 6), 1);
+	else
+		change_dir(args[0], 0);
+}
+
 void	execute_command(char **command)
 {
 	if (!command || !*command || !**command)
@@ -240,7 +291,8 @@ void	execute_command(char **command)
 		set_env(command + 1);
 	else if (!ft_strcmp(command[0], "unsetenv"))
 		unset_env(command + 1);
-	// else if (!ft_strcmp(command[0], "cd"))
+	else if (!ft_strcmp(command[0], "cd"))
+		cd_command(command + 1);
 }
 
 void	execute_commands(char **commands)
