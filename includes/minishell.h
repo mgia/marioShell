d3b32e6e36183293750218cfcd3faea9da5c630a
Marc G Tan <findmarcg@gmail.com>
2018-11-06 21:29:11 -0500

    Modified minishell.h to split files

--- includes/minishell.h
+++ includes/minishell.h
@@ -11,3 +11,44 @@
 
 char	**g_envp;
 #endif
+
+/*
+**	Fork functions
+*/
+
+int		process(char *ex, char **args);
+int		check_bins(char **args);
+int		system_call(char **args);
+
+/*
+**	Builtin functions
+*/
+
+void	change_dir(char *path, int print);
+void	cd(char **args);
+void	echo(char **args);
+void	set_env(char **arg);
+void	unset_env(char **args);
+
+
+/*
+**	Environment Management functions
+*/
+
+void	init_env(char **envp);
+void	print_env(void);
+char	**realloc_env(int len);
+void	set_envp(char *key, char *value, int print);
+void	unset_envp(int pos);
+char	*find_arg(char *key, int len);
+int		find_envp(char *key);
+
+/*
+**	Utilities
+*/
+
+void	display_cwd(void);
+void	signal_handler(int n);
+void	ft_freestrarr(char **arr);
+void	exit_shell(void);
+void	mario(void);
